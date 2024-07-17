c3289aedc167971c2497630f82e95ec08d4d8268
Estevam Gomes Dalmoro <73229133+estevamgd@users.noreply.github.com>
2024-07-17 12:06:15 -0300

    Working

--- hbt_analysis_perpheralPbPb.C
+++ hbt_analysis_perpheralPbPb.C
@@ -4,7 +4,6 @@
 
 
 void hbt_analysis_perpheralPbPb() {
-
     // Open the input file
     TFile* fr = new TFile("HiForestAOD_UPC.root", "READ");
 
@@ -12,46 +11,42 @@ void hbt_analysis_perpheralPbPb() {
     TTree* t;
     fr->GetObject("demo/HBT", t);
 
-    /*Declare variables to read from the tree: HFsumET, 
-    Ntrk, trkPt, trkEta, trkPhi, trkPtRes, trkDzSig, trkDxySig, 
-    trkNpixLayers, qinvSigSS, coulombWSS, qinvSigOS, coulombWOS*/ 
+    /* Variables to read from the tree: HFsumET, Ntrk, trkPt, trkEta, trkPhi, trkPtRes, 
+    trkDzSig, trkDxySig, trkNpixLayers, qinvSigSS, coulombWSS, qinvSigOS, coulombWOS*/ 
+    const char* varNames[] = {"HFsumET", "Ntrk", "trkPt", "trkEta", "trkPhi", "trkPtRes", "trkDzSig", 
+    "trkDxySig", "trkNpixLayers", "qinvSigSS", "coulombWSS", "qinvSigOS", "coulombWOS"};
+
+    Int_t nNames = sizeof(varNames);
     
-    Float_t HFsumET, trkPt, trkEta, trkPhi, trkPtRes, trkDzSig, trkDxySig, 
-    trkNpixLayers, qinvSigSS, coulombWSS, qinvSigOS, coulombWOS;
-
-    Int_t Ntrk;
-
-    // Set branches
-    t->SetBranchAddress("HFsumET", &HFsumET);
-    t->SetBranchAddress("Ntrk", &Ntrk);
-    t->SetBranchAddress("trkPt", &trkPt);
-    t->SetBranchAddress("trkEta", &trkEta);
-    t->SetBranchAddress("trkPhi", &trkPhi);
-    t->SetBranchAddress("trkPtRes", &trkPtRes);
-    t->SetBranchAddress("trkDzSig", &trkDzSig);
-    t->SetBranchAddress("trkDxySig", &trkDxySig);
-    t->SetBranchAddress("trkNpixLayers", &trkNpixLayers);
-    t->SetBranchAddress("qinvSigSS", &qinvSigSS);
-    t->SetBranchAddress("coulombWSS", &coulombWSS);
-    t->SetBranchAddress("qinvSigOS", &qinvSigOS);
-    t->SetBranchAddress("coulombWOS", &coulombWOS);
-
     // Create histograms
-    TCanvas* c1 = new TCanvas("c1", "Histograms part 1", 1280, 720);
-    c1->Divide(2,3);
+    TCanvas* c1 = new TCanvas("c1", "Histograms part 1", 1280, 720); // Canvas 1
+    c1->Divide(3,2);
 
-    c1->cd(1); t->Draw("HFsumET"); 
-    c1->cd(2); t->Draw("Ntrk"); 
-    c1->cd(3); t->Draw("trkPt"); 
+    for (int i = 1; i < 7; i++) {
+        c1->cd(i); t->Draw(varNames[i-1]);
+    }
+
+    TCanvas* c2 = new TCanvas("c2", "Histograms part 2", 1280, 720); // Canvas 2
+    c2->Divide(3,2);
+
+    for (int i = 1; i < 7; i++) {
+        c2->cd(i); t->Draw(varNames[i+6]);
+    }
+
+    TCanvas* c3 = new TCanvas("c3", "Histograms part 3", 1280, 720); // Canvas 3
+    c3->cd(1); t->Draw("coulombWOS");
     
-    TCanvas* c2 = new TCanvas("c2", "Histograms part 2", 1280, 720);
-    c2->Divide(2,4);
 
     // Save the canvas
+    c1->SaveAs("./imgs/img-11.eps");
+    c2->SaveAs("./imgs/img-22.eps");
+    c3->SaveAs("./imgs/img-33.eps");
 
     // Close the file
+    c1->Close();
+    c2->Close();
+    c3->Close();
+    
     fr->Close();
 
-    // Clean up memory
-    
 }
