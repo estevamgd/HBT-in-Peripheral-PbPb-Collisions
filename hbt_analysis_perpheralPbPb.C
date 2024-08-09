3552c84bb7d2b1531580c10ce10b344bb9bc0906
Estevam Gomes Dalmoro <73229133+estevamgd@users.noreply.github.com>
2024-08-09 20:06:33 -0300

    Add files via upload

--- hbt_analysis_perpheralPbPb.C
+++ hbt_analysis_perpheralPbPb.C
@@ -1,52 +1,46 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "TCanvas.h"
-
+#include "TH1D.h"
+#include <stdio.h>
+#include <stdbool.h>
 
 void hbt_analysis_perpheralPbPb() {
     // Open the input file
-    TFile* fr = new TFile("HiForestAOD_UPC.root", "READ");
+    TFile *fr = new TFile("HiForestAOD_UPC.root", "READ");
+    if (!fr || fr->IsZombie()) {
+        std::cerr << "Error: File could not be opened!" << std::endl;
+        return;
+    }
 
     // Get the tree from demo/HBT
-    TTree* t;
-    fr->GetObject("demo/HBT", t);
-
-    /* Variables to read from the tree: HFsumET, Ntrk, trkPt, trkEta, trkPhi, trkPtRes, 
-    trkDzSig, trkDxySig, trkNpixLayers, qinvSigSS, coulombWSS, qinvSigOS, coulombWOS*/ 
-    const char* varNames[] = {"HFsumET", "Ntrk", "trkPt", "trkEta", "trkPhi", "trkPtRes", "trkDzSig", 
-    "trkDxySig", "trkNpixLayers", "qinvSigSS", "coulombWSS", "qinvSigOS", "coulombWOS"};
-
-    Int_t nNames = sizeof(varNames);
-    
-    // Create histograms
-    TCanvas* c1 = new TCanvas("c1", "Histograms part 1", 1280, 720); // Canvas 1
-    c1->Divide(3,2);
-
-    for (int i = 1; i < 7; i++) {
-        c1->cd(i); t->Draw(varNames[i-1]);
+    TTree *t;
+    fr->GetObject("demo/HBT;570", t);
+    if (!t) {
+        std::cerr << "Error: Tree 'demo/HBT;570' not found!" << std::endl;
+        return;
     }
 
-    TCanvas* c2 = new TCanvas("c2", "Histograms part 2", 1280, 720); // Canvas 2
-    c2->Divide(3,2);
+    Float_t HFsumET, coulombWOS;
 
-    for (int i = 1; i < 7; i++) {
-        c2->cd(i); t->Draw(varNames[i+6]);
+    t->SetBranchAddress("HFsumET", &HFsumET);
+    t->SetBranchAddress("coulombWOS", &coulombWOS);
+
+    Long64_t nentries = t->GetEntries();
+
+    TCanvas *c1 = new TCanvas("c1", "Histograms part 1", 1280, 720);
+    c1->Divide(1,2);
+
+    TH1D *h1 = new TH1D("h1", "HFsumET Histogram", 10000, 0, 400);
+    TH1D *h2 = new TH1D("h2", "coulombWOS Histogram", 1000, 0, 2);
+
+    for (int i=0;i<nentries;i++) {
+        t->GetEntry(i);
+        h1->Fill(HFsumET);
+        h2->Fill(coulombWOS);
     }
 
-    TCanvas* c3 = new TCanvas("c3", "Histograms part 3", 1280, 720); // Canvas 3
-    c3->cd(1); t->Draw("coulombWOS");
+    c1->cd(1); h1->Draw();
+    c1->cd(2); h2->Draw();
     
-
-    // Save the canvas
-    c1->SaveAs("./imgs/img-11.eps");
-    c2->SaveAs("./imgs/img-22.eps");
-    c3->SaveAs("./imgs/img-33.eps");
-
-    // Close the file
-    c1->Close();
-    c2->Close();
-    c3->Close();
-    
-    fr->Close();
-
 }
